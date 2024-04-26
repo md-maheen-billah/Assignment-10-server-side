0fe2d96377e07bfd39e7e4c7e9a5eb47faeb6edd
md-maheen-billah <md.maheen.billah@icloud.com>
2024-04-26 21:18:50 +0200

    implemented my art and craft list functionality in server side using get operation for specific email

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ async function run() {
       const result = await itemCollection.insertOne(req.body);
       res.send(result);
     });
+
+    app.get("/mylist/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await itemCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
